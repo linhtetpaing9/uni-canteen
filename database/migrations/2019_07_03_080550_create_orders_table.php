b51fe87d614927edf38bed8c57a6096dd5715f80
Lin Htet Paing <linhtetpaing9@gmail.com>
2019-07-03 15:11:45 +0630

    initial setup

---
+++ database/migrations/2019_07_03_080550_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('item_id');
+            $table->enum('status', ['DELIVERY', 'IN_SHOP']);
+            $table->integer('quantity');
+            $table->time('time_to_eat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
